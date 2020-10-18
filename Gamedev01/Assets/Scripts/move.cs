7cc752ef00eb397429e6cfd4a725f426139b5bd9
plywakd <46456311+plywakd@users.noreply.github.com>
2020-10-18 12:22:13 +0200

    Gamedev 01 finished
    First lab finished, added project for 2d game

---
+++ Gamedev01/Assets/Scripts/move.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+
+    private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 vec = new Vector2(moveHorizontal, moveVertical);
+        print("INPUT VECTOR:" + vec);
+        // rb.AddForce(vec * 10.0f);
+        rb.velocity = vec;
+        //rb.MovePosition(vec);
+    }
+}
---
+++ Gamedev01/Assets/Scripts/rotate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    public GameObject poly;
+    private Rigidbody2D skeleton;
+    private int tmp = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        skeleton = gameObject.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (tmp < 4000)
+        {
+            transform.Rotate(new Vector3(0, 0, 1));
+            // transform.localScale += new Vector3(0.1f, 0.1f, 0.1f);
+            // transform.Translate(new Vector3(0.1f, 0, 0));
+            tmp++;
+        }
+    }
+}
