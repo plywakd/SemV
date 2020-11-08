43f01002524302f8ab3a9002de7090c272ee289c
plywakd <46456311+plywakd@users.noreply.github.com>
2020-11-08 16:06:00 +0100

    MobileLab04 init
    Prepared entry files for canvas lab on android

---
+++ MobileLab04/app/src/main/java/com/mobilelab04/Task03Activity.java
@@ -0,0 +1,14 @@
+package com.mobilelab04;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Task03Activity extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.task03);
+    }
+}
