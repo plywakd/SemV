8cf4a2f8bb1ecddea3db26fa32d12df1c4551152
plywakd <46456311+plywakd@users.noreply.github.com>
2020-11-28 20:45:58 +0100

    Oop Project Web app using REST oopProj app init

---
+++ oopProjWeb/oopProjWeb/src/main/java/com/oopProjWeb/OopProjWebApplication.java
@@ -0,0 +1,13 @@
+package com.oopProjWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopProjWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopProjWebApplication.class, args);
+	}
+
+}
