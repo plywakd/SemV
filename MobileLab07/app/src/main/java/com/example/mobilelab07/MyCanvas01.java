561899d240db96fbe52f7a3fa076e13de7a97817
plywakd <46456311+plywakd@users.noreply.github.com>
2020-12-06 17:09:35 +0100

    MobileLab07 snow particles stacking and regenerating after fall

--- MobileLab07/app/src/main/java/com/example/mobilelab07/MyCanvas01.java
+++ MobileLab07/app/src/main/java/com/example/mobilelab07/MyCanvas01.java
@@ -27,14 +27,14 @@ public class MyCanvas01 extends View {
     private Random rand = new Random();
     private List<CanvCircle> snow = new ArrayList<>();
     private boolean created = false;
-    public int counter = 0;
+    public int counter;
     Paint paint = new Paint();
 
 
     public MyCanvas01(Context context) {
         super(context);
         c = context;
-        createSnowParticles();
+        createSnowParticles(100);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
@@ -50,12 +50,12 @@ public class MyCanvas01 extends View {
         invalidate();
     }
 
-    public void createSnowParticles() {
-        for (int i = 0; i < 100; i++) {
+    public void createSnowParticles(int quantity) {
+        for (int i = 0; i < quantity; i++) {
             int cx = rand.nextInt(1080);
             int cy = rand.nextInt(300);
             int r = rand.nextInt(9) + 3;
-            int gravity = rand.nextInt(10) + 1;
+            int gravity = rand.nextInt(9) + 3;
             snow.add(new CanvCircle(cx, cy, r, gravity, true));
         }
     }
@@ -68,11 +68,14 @@ public class MyCanvas01 extends View {
                 if (particle.getCy() < height - particle.getR()) {
                     moveSnow(particle);
                 } else {
+                    counter++;
                     particle.setFalling(false);
                 }
             }
             canvas.drawCircle(particle.getCx(), particle.getCy(), particle.getR(), paint);
         }
+        createSnowParticles(counter);
+        counter = 0;
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
@@ -84,7 +87,7 @@ public class MyCanvas01 extends View {
                 .filter(p -> p.getCy() - (particle.getCy() + (particle.getR() / 2)) <= p.getR())
                 .findAny()).orElse(null);
         if (collision.isPresent()) {
-            System.out.println("col element: " + collision);
+            counter++;
             particle.setFalling(false);
         } else {
             particle.setCy(deltaCy);
