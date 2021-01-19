9b7f9feda963a793be4052e956a91a0c399f344d
plywakd <46456311+plywakd@users.noreply.github.com>
2021-01-19 14:44:15 +0100

    lombok added for models

--- oopProjWeb/oopProjWeb/src/main/java/com/oopProjWeb/model/Student.java
+++ oopProjWeb/oopProjWeb/src/main/java/com/oopProjWeb/model/Student.java
@@ -2,10 +2,12 @@ package com.oopProjWeb.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -19,59 +21,4 @@ public class Student {
     private boolean landline;
     private Set<Project> projects;
 
-    public Integer getStudentId() {
-        return studentId;
-    }
-
-    public void setStudentId(Integer studentId) {
-        this.studentId = studentId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getIndexNum() {
-        return indexNum;
-    }
-
-    public void setIndexNum(String indexNum) {
-        this.indexNum = indexNum;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public boolean isLandline() {
-        return landline;
-    }
-
-    public void setLandline(boolean landline) {
-        this.landline = landline;
-    }
-
-    public Set<Project> getProjects() {
-        return projects;
-    }
-
-    public void setProjects(Set<Project> projects) {
-        this.projects = projects;
-    }
 }
--- oopProjWeb/oopProjWeb/src/main/java/com/oopProjWeb/model/Task.java
+++ oopProjWeb/oopProjWeb/src/main/java/com/oopProjWeb/model/Task.java
@@ -2,11 +2,13 @@ package com.oopProjWeb.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -19,54 +21,6 @@ public class Task {
     private String description;
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS")
     private LocalDateTime addedDate;
-
-    public Integer getTaskId() {
-        return taskId;
-    }
-
-    public void setTaskId(Integer taskId) {
-        this.taskId = taskId;
-    }
-
-    public Project getProject() {
-        return project;
-    }
-
-    public void setProject(Project project) {
-        this.project = project;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getTaskOrder() {
-        return taskOrder;
-    }
-
-    public void setTaskOrder(Integer taskOrder) {
-        this.taskOrder = taskOrder;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDateTime getAddedDate() {
-        return addedDate;
-    }
-
-    public void setAddedDate(LocalDateTime addedDate) {
-        this.addedDate = addedDate;
-    }
 
     @Override
     public String toString() {
